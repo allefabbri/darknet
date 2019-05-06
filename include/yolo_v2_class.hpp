bcf4f70b52875c3fb6ca3f811729fdaba71f0561
Alex <baroncini.alex@gmail.com>
2019-05-06 17:29:54 +0200

    restore previous version

--- include/yolo_v2_class.hpp
+++ include/yolo_v2_class.hpp
@@ -99,23 +99,6 @@ public:
         return detection_boxes;
     }
 
-    std::vector<bbox_t> detect_resized_types(image_t img, int init_w, int init_h, const std::vector<int> &types_idx, float thresh = 0.2, bool use_mean = false)
-    {
-        if (img.data == NULL)
-            throw std::runtime_error("Image is empty");
-        auto detection_boxes = detect(img, thresh, use_mean);
-        for (auto it = detection_boxes.begin(); it != detection_boxes.end();)
-        {
-          if ( std::any_of(types_idx.begin(), types_idx.end(), [it](int idx){ return it->obj_id == idx; }) )
-            ++it;
-          else
-            it = detection_boxes.erase(it);
-        }
-        float wk = (float)init_w / img.w, hk = (float)init_h / img.h;
-        for (auto &i : detection_boxes) i.x *= wk, i.w *= wk, i.y *= hk, i.h *= hk;
-        return detection_boxes;
-    }
-
 #ifdef OPENCV
     std::vector<bbox_t> detect(cv::Mat mat, float thresh = 0.2, bool use_mean = false)
     {
